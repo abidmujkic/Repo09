53fd3e8bac3c6743379cd1302a4745a8245bf227
abidmujkic <abidmujkic1@gmail.com>
2021-09-22 08:53:27 +0200

    fix

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,7 +1,8 @@
-//
+// Only change code below this line
 function myTrueBoolean() {
     return true;
 }
-//
+// Only change code above this line
+
 console.log(myTrueBoolean());
 module.exports = myTrueBoolean;
